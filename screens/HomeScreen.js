662e5ba8981821eb456e502fdb70a8e3d9acdab7
mathewacarter29 <mathewacarter29@vt.edu>
2023-03-27 18:10:59 -0400

    Added a logout button and simplified styling

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,50 +1,51 @@
-import {React, useState} from "react";
+import { React, useState } from "react";
 
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import Container from "../common/Container";
 import EStyleSheet, { absoluteFill } from "react-native-extended-stylesheet";
 
-
 function HomeScreen({ navigation }) {
-
-    return(
-        <Container>
-            <View style={styles.container}>
-                <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("Profile")}>
-                    <Image
-                        style={styles.image}
-                        source={require("../assets/profile_icon.png")} />
-                </TouchableOpacity>
-                <Text style={styles.text}>Home Screen</Text>
-                <Image source={require("../assets/basketballPlayerArms.png")} />
-            </View>
-            <View style={{ flex: 1, backgroundColor: "lightgray" }}></View>
-        </Container>
-    );
+  return (
+    <Container>
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("Profile")}
+        >
+          <Image
+            style={styles.image}
+            source={require("../assets/profile_icon.png")}
+          />
+        </TouchableOpacity>
+        <Text style={styles.text}>Home Screen</Text>
+        <Image source={require("../assets/basketballPlayerArms.png")} />
+      </View>
+    </Container>
+  );
 }
 
 const styles = EStyleSheet.create({
-    container: {
-        alignItems: "center",
-        paddingTop: "2rem",
-        justifyContent: "flex-end",
-    },
-    text: {
-        margin: "1rem",
-        marginTop: "7rem",
-        fontSize: 30,
-        width: "80%",
-        textAlign: "center",
-    },
-    image: {
-        width: 50,
-        height: 52,
-    },
-    button: {
-        position: "absolute",
-        right: "8%",
-        top: "13%",
-    }
+  container: {
+    alignItems: "center",
+    paddingTop: "2rem",
+    justifyContent: "flex-end",
+  },
+  text: {
+    margin: "1rem",
+    marginTop: "7rem",
+    fontSize: 30,
+    width: "80%",
+    textAlign: "center",
+  },
+  image: {
+    width: 50,
+    height: 52,
+  },
+  button: {
+    position: "absolute",
+    right: "8%",
+    top: "13%",
+  },
 });
 
 export default HomeScreen;
--- screens/Profile.js
+++ screens/Profile.js
@@ -1,94 +1,85 @@
 import EStyleSheet from "react-native-extended-stylesheet";
 import Container from "../common/Container";
 import { View, Text, Image, TouchableOpacity } from "react-native";
-import { Auth } from "aws-amplify"
+import { Auth } from "aws-amplify";
 import LoadingScreen from "../common/LoadingScreen";
 import { React, useState, useEffect } from "react";
+import Button from "../common/Button";
 
+function Profile({ navigation }) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
 
-
-
-function Profile({ navigation}) {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [loading, setLoading] = useState(false);
-    
-    async function getProfileData() {
-        setLoading(true);
-        try {
-            response = await Auth.currentUserInfo()
-            setName(response.attributes.name)
-            setEmail(response.attributes.email)
-            setLoading(false);
-        }
-        catch (e) {
-            setLoading(false);
-            return;
-        }
+  async function getProfileData() {
+    setLoading(true);
+    try {
+      response = await Auth.currentUserInfo();
+      setName(response.attributes.name);
+      setEmail(response.attributes.email);
+    } catch (e) {
+      // we dont do anything with exceptions right now
     }
+    setLoading(false);
+  }
 
-    useEffect( () => {
-        getProfileData()
-    }, [])
+  async function logOut() {
+    setLoading(true);
+    try {
+      response = await Auth.signOut({ global: true });
+      navigation.navigate("GetStarted");
+    } catch (e) {
+      setLoading(false);
+    }
+  }
 
-    return (
-        <Container>
-            {loading && <LoadingScreen/>}
-            <View style={styles.container}>
-                <TouchableOpacity style={styles.back} onPress={() => navigation.navigate("HomeScreen")}>
-                    <Image
-                        style={styles.image}
-                        source={require("../assets/back_arrow_icon.png")} />
-                </TouchableOpacity>
-                <Text style={styles.text}>Profile</Text>
-                <Image source={require("../assets/profile_icon.png")} />
-            </View>
-            <View style={styles.first_info}>
-                <Text style={styles.text_info}>Name: {name}</Text>
-                <Text style={styles.text_info}>Email: {email}</Text>
-            </View>
+  useEffect(() => {
+    getProfileData();
+  }, []);
 
-        </Container>
-        );
+  return (
+    <Container>
+      {loading && <LoadingScreen />}
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={styles.back}
+          onPress={() => navigation.navigate("HomeScreen")}
+        >
+          <Image source={require("../assets/back_arrow_icon.png")} />
+        </TouchableOpacity>
+        <Text style={styles.text}>Profile</Text>
+        <Image source={require("../assets/profile_icon.png")} />
+        <Text style={styles.text_info}>Name: {name}</Text>
+        <Text style={styles.text_info}>Email: {email}</Text>
+        <Button title="Log Out" onPress={() => logOut()} />
+      </View>
+    </Container>
+  );
 }
 
 const styles = EStyleSheet.create({
-    container: {
-        alignItems: "center",
-        paddingTop: "1rem",
-        justifyContent: "center",
-    },
-    text: {
-        margin: "1rem",
-        marginTop: "6rem",
-        fontSize: 30,
-        width: "80%",
-        textAlign: "center",
-    },
-    back: {
-        position: "absolute",
-        left: "0%",
-        top: "0%",
-    },
-    image: {
-        marginTop: "3rem",
-        marginLeft: "2rem",
-    },
-    first_info: {
-        marginLeft: "2rem",
-        marginTop: "2rem",
-        width: "100%",
-        flex: 1,
-        flexDirection: "row",
-        flexWrap: "wrap",
-        alignItems: "flex-start",
-    },
-    text_info: {
-        margin: ".5rem",
-        fontSize: 20,
-        width: "100%",
-    },
-})
+  container: {
+    alignItems: "center",
+    paddingTop: "1rem",
+    justifyContent: "center",
+  },
+  text: {
+    margin: "1rem",
+    marginTop: "6rem",
+    fontSize: 30,
+    width: "80%",
+    textAlign: "center",
+  },
+  back: {
+    position: "absolute",
+    left: "7%",
+    top: "12%",
+  },
+  text_info: {
+    margin: ".5rem",
+    fontSize: 20,
+    width: "80%",
+  },
+});
 
 export default Profile;
-
