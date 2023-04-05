70a16ff61cd7f6eb3985e826b29a23f13aed29d8
AbirRahaman <60662582+AbirRahaman@users.noreply.github.com>
2023-04-04 21:42:33 -0400

    working on deleting a game

--- screens/GameDetails.js
+++ screens/GameDetails.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import EStyleSheet from "react-native-extended-stylesheet";
 import { View, Text } from "react-native";
 import Container from "../common/Container";
 import Button from "../common/Button";
+import { DataStore, Auth } from "aws-amplify";
+import LoadingScreen from "../common/LoadingScreen";
+import { Game, Player } from "../src/models";
 
 function epochToLocalDate(epoch) {
     const date = new Date(epoch * 1000); // Convert epoch to milliseconds
@@ -22,15 +25,33 @@ function epochToLocalDate(epoch) {
   }
 
 function GameDetails({ route, navigation }) {
+  const [loading, setLoading] = useState(false);
+
   console.log("route params, ", route.params.item);
   const details = route.params.item;
   console.log("details: ", details);
   const DUMMY_USERNAME = "Abir Rahaman";
 
-  function isGameOwner() {
+    //gets current user
+    async function getPlayer() {
+        try {
+          const response = await Auth.currentUserInfo();
+          const player = await DataStore.query(Player, (p) =>
+            p.email.eq(response.attributes.email)
+          );
+          return player[0];
+        } catch (error) {
+          setShowError(true);
+          setErrorMessage(`Error getting player: ${error.message}`);
+          console.log("Error getting player: ", error.message);
+        }
+      }
+
+  async function isGameOwner() {
     // Use a dummy username until actual organizer names are used
     // We would need DUMMY_USERNAME to be the name of the current user
-    return DUMMY_USERNAME == details.organizer;
+    const player = await getPlayer();
+    return player.id == details.organizer;
   }
 
   function showDescription() {
@@ -42,9 +63,20 @@ function GameDetails({ route, navigation }) {
         game: route.params.item,
       });
   }
+ 
+  async function handleDelete() {
 
+    //setLoading(true);
+
+    const toDelete = await DataStore.query(Game, details.id);
+    DataStore.delete(toDelete);
+
+    //setLoading(false);
+    navigation.navigate("HomeScreen");
+  }
   return (
     <Container goBackTo="HomeScreen">
+    {loading && <LoadingScreen />}
       <View style={styles.container}>
         <View style={{ alignItems: "center" }}>
           <Text style={styles.topText}>Game details for {details.name}</Text>
@@ -73,7 +105,7 @@ function GameDetails({ route, navigation }) {
         </View>
         <View style={styles.buttonContainer}>
           <Button title="Edit Game Details" disabled={!isGameOwner()} onPress={handleEdit} />
-          <Button title="Delete Game" disabled={!isGameOwner()} />
+          <Button title="Delete Game" disabled={!isGameOwner()} onPress={handleDelete} />
         </View>
       </View>
     </Container>
