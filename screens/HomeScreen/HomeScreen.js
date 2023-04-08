c718d169d49c9279bff95b4489afc0414289464c
rmurudkar <rmurudkar@vt.edu>
2023-04-07 21:57:05 -0400

    Update HomeScreen.js

--- screens/HomeScreen/HomeScreen.js
+++ screens/HomeScreen/HomeScreen.js
@@ -20,23 +20,6 @@ import { Player, Game, Location, GamePlayer, Rsvp } from "../../src/models";
 import "@azure/core-asynciterator-polyfill";
 import Container from "../../common/Container";
 
-function epochToLocalDate(epoch) {
-  const date = new Date(epoch * 1000); // Convert epoch to milliseconds
-  const month = ('0' + (date.getMonth() + 1)).slice(-2); // Get local month and pad with 0 if needed
-  const day = ('0' + date.getDate()).slice(-2); // Get local day and pad with 0 if needed
-  const year = date.getFullYear(); // Get local year
-  return `${month}-${day}-${year}`;
-}
-
- function epochToLocalTime(epoch) {
-  const date = new Date(epoch * 1000); // Convert epoch to milliseconds
-  const hours = date.getHours(); // Get local hours
-  const minutes = ('0' + date.getMinutes()).slice(-2); // Get local minutes and pad with 0 if needed
-  const ampm = hours >= 12 ? 'PM' : 'AM';
-  const formattedHours = ('0' + (hours % 12 || 12)).slice(-2); // Convert to 12-hour format and pad with 0 if needed
-  return `${formattedHours}:${minutes} ${ampm}`;
-}
-
 function HomeScreen({ navigation }) {
   const [games, setGames] = useState([]);
   const [userGames, setUserGames] = useState([]);
@@ -70,8 +53,7 @@ function HomeScreen({ navigation }) {
     });
     console.log("ALL GAMES: ", allGames);
     //console.log("user game ids: ", userGameIds);
-    const userGames = allGames.filter((game) => {
-      // console.log("game id: ", game.id);
+    const userGames = allGames.filter((game) => {          
       return userGameIds.includes(game.id);
     });
      console.log("userGames: ", userGames);
