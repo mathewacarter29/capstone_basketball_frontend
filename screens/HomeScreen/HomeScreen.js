c23e098cb57799cad59d389fd71b7568b67b5770
mathewacarter29 <mathewacarter29@vt.edu>
2023-04-10 20:03:47 -0400

    HomeScreen fixes complete

--- screens/HomeScreen/HomeScreen.js
+++ screens/HomeScreen/HomeScreen.js
@@ -12,13 +12,11 @@ import EStyleSheet from "react-native-extended-stylesheet";
 import LoadingScreen from "../../common/LoadingScreen";
 
 import GameFeed from "./GameFeed";
-import Button from "../../common/Button";
 
 import { Auth } from "aws-amplify";
 import { DataStore } from "aws-amplify";
 import { Player, Game, Location, GamePlayer, Rsvp } from "../../src/models";
 import "@azure/core-asynciterator-polyfill";
-import Container from "../../common/Container";
 
 function HomeScreen({ navigation }) {
   const [games, setGames] = useState([]);
@@ -26,56 +24,63 @@ function HomeScreen({ navigation }) {
   const [playerGames, setPlayerGames] = useState([]);
   const [loading, setLoading] = useState(false);
   const [thisPlayer, setThisPlayer] = useState([]);
-  
-  
+
   async function getPlayerGames(allGames) {
+    // NEED TRY CATCH AROUND ALL API CALLS
+    try {
+      const authObj = await Auth.currentUserInfo();
+      const userEmail = authObj.attributes.email;
 
-    const authObj = await Auth.currentUserInfo();
-    // console.log("Auth Object returned: ", authObj);
-    const userEmail = authObj.attributes.email;
-  
-    // get Player object from email
-    const players = await DataStore.query(Player, (p) =>
-      p.email.eq(userEmail)
-    );
-    const player = players[0];
-    setThisPlayer(player);
-    // console.log("Player returned: ", player);
+      // get Player object from email
+      const players = await DataStore.query(Player, (p) =>
+        p.email.eq(userEmail)
+      );
+      const player = players[0];
+      setThisPlayer(player);
 
-    // console.log("Player id: ", player.id);
-  
-    // query GamePlayer to find games player is invited to or created
-    const gamePlayers = await DataStore.query(GamePlayer, (gp) =>
-      gp.player_id.eq(player.id)
-    );
-    //console.log("Game players: ", gamePlayers);
-    const userGameIds = gamePlayers.map((gamePlayer) => {
-      return gamePlayer.id
-    });
-    console.log("ALL GAMES: ", allGames);
-    //console.log("user game ids: ", userGameIds);
-    const userGames = allGames.filter((game) => {          
-      return userGameIds.includes(game.id);
-    });
-     console.log("userGames: ", userGames);
-  
-     setPlayerGames(userGames);
-  
+      // query GamePlayer to find games player is invited to or created
+      const gamePlayers = await DataStore.query(GamePlayer, (gp) =>
+        gp.player_id.eq(player.id)
+      );
+      //console.log("Game players: ", gamePlayers);
+      const userGameIds = gamePlayers.map((gamePlayer) => {
+        return gamePlayer.id;
+      });
+      console.log("ALL GAMES: ", allGames);
+      //console.log("user game ids: ", userGameIds);
+      const userGames = allGames.filter((game) => {
+        return userGameIds.includes(game.id);
+      });
+      console.log("userGames: ", userGames);
+
+      setPlayerGames(userGames);
+    } catch (error) {
+      setLoading(false);
+      console.log(error.message);
+    }
   }
 
   useEffect(() => {
     /**
      * This keeps `Games` fresh.
-     * if another user makes a change to the game details, 
+     * if another user makes a change to the game details,
      * we will get that change reflected here with our subscriber
      */
-    const subscriber = DataStore.observeQuery(Game, (c) =>
-      c.datetime.gt(Math.floor(Date.now() / 1000))
-    ).subscribe(({ items }) => {
-      console.log("items:" ,  items);
-      setGames(items);
-      setUserGames(getPlayerGames(items));
-    });
+    // TRY CATCH AROUND API CALLS
+    try {
+      setLoading(true);
+      // LOAD WHILE PERFORMING API CALLS
+      const subscriber = DataStore.observeQuery(Game, (c) =>
+        c.datetime.gt(Math.floor(Date.now() / 1000))
+      ).subscribe(({ items }) => {
+        console.log("items:", items);
+        setGames(items);
+        setUserGames(getPlayerGames(items));
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+    setLoading(false);
 
     return () => {
       subscriber.unsubscribe();
@@ -97,7 +102,7 @@ function HomeScreen({ navigation }) {
       </TouchableOpacity>
       <TouchableOpacity
         style={styles.createButton}
-        onPress={() => navigation.navigate("CreateGame", {thisPlayer})}
+        onPress={() => navigation.navigate("CreateGame", { thisPlayer })}
       >
         <Image
           style={styles.image}
@@ -105,7 +110,7 @@ function HomeScreen({ navigation }) {
         />
       </TouchableOpacity>
       <View style={styles.innerContainer}>
-        <GameFeed data={{games: games, thisPlayer: thisPlayer}} />
+        <GameFeed data={{ games: games, thisPlayer: thisPlayer }} />
       </View>
     </View>
   );
