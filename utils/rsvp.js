d28ef1a028484d3e9f3ceb09f8d20ee85aa06b43
mathewacarter29 <mathewacarter29@vt.edu>
2023-04-14 19:02:59 -0400

    Safety for rsvp function

--- utils/rsvp.js
+++ utils/rsvp.js
@@ -9,53 +9,41 @@ import {
 import { DataStore } from "aws-amplify";
 import "@azure/core-asynciterator-polyfill";
 
-
 async function rsvp(gameId, playerId, newRsvp) {
-  const original = await DataStore.query(GamePlayer, (c) => c.and(c => [
-    c.game_id.eq(gameId),
-    c.player_id.eq(playerId)
-  ]));
+  try {
+    // player's original RSVP
+    const original = await DataStore.query(GamePlayer, (c) =>
+      c.and((c) => [c.game_id.eq(gameId), c.player_id.eq(playerId)])
+    );
+    console.log(original);
 
-  console.log("original: ", original);
-  
-  if (original.length > 0 && original[0].rsvp != newRsvp) {
-    try {
+    // if the player has RSVPd before or invited
+    if (original.length > 0 && original[0].rsvp != newRsvp) {
       const updatedGamePlayer = await DataStore.save(
-        GamePlayer.copyOf(original[0], updated => {
-          updated.rsvp = newRsvp
+        GamePlayer.copyOf(original[0], (updated) => {
+          updated.rsvp = newRsvp;
         })
       );
 
       return true;
     }
 
-    catch (error) {
-
-      console.log("error saving rsvp: ", error);
-      return false;
+    // user rsvp to a game he was not invited to
+    else if (original.length == 0) {
+      const gamePlayer = await DataStore.save(
+        new GamePlayer({
+          player_id: playerId,
+          game_id: gameId,
+          rsvp: newRsvp,
+          invited: false,
+        })
+      );
+      console.log("game player stored: ", gamePlayer);
     }
+  } catch (error) {
+    console.log("error retrieving game player: ", error.message);
+    return false;
   }
-
-  // user rsvp to a game he was not invited to
-  else if (original.length == 0){
-    try {
-      if (newRsvp == Rsvp.ACCEPTED) {
-        const gamePlayer = await DataStore.save(
-          new GamePlayer({
-            player_id: playerId,
-            game_id: gameId,
-            rsvp: newRsvp,
-            invited: false,
-          })
-        );
-        console.log("game player stored: ", gamePlayer);
-      }
-      
-    } catch (error) {
-      console.log("error: ", error.message, "storing player: ", playerId);
-    }
-  }
-  
 }
 
 export default rsvp;
