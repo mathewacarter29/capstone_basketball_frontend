f231f3bbb2f284c4958048f7b25477021a84925a
rmurudkar <rmurudkar@vt.edu>
2023-04-19 22:14:05 -0400

    Update update game: allow to invite more players, also preset the date to the current date of the game

--- screens/UpdateGame.js
+++ screens/UpdateGame.js
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from "react";
 import { View, Text } from "react-native";
 import EStyleSheet, { value } from "react-native-extended-stylesheet";
 import Button from "../common/Button";
-import LoadingScreen from "../common/LoadingScreen";
 import TextInput from "../common/TextInput";
 import Container from "../common/Container";
 import RNPickerSelect from "react-native-picker-select";
 import RNDateTimePicker from "@react-native-community/datetimepicker";
+import { MultiSelect } from "react-native-element-dropdown";
 import { DataStore, Auth } from "aws-amplify";
 import {
   Player,
@@ -17,6 +17,8 @@ import {
   SkillLevel,
 } from "../src/models";
 import ErrorPopup from "../common/ErrorPopup";
+// import getPlayers from "../utils/Players";
+// import getLocations from "../utils/Locations";
 import "@azure/core-asynciterator-polyfill";
 
 function UpdateGame({ route, navigation }) {
@@ -24,12 +26,66 @@ function UpdateGame({ route, navigation }) {
   const player = route.params.player;
   const [loading, setLoading] = useState(false);
   const [gameName, setGameName] = useState(game.name);
+  const [toInvite, setToInvite] = useState([]);
   const [gameDescription, setGameDescription] = useState(game.description);
+  const [locations, setLocations] = useState([]);
   const [gameLocation, setLocation] = useState("");
   const [gameSkillLevel, setSkillLevel] = useState(game.skill_level);
-  const [chosenDate, setChosenDate] = useState(new Date());
+  const [chosenDate, setChosenDate] = useState(new Date(game.datetime * 1000));
   const [showError, setShowError] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [selectedPlayers, setSelected] = useState(game.invited_players);
+
+  //get all locations to populate the location picker
+  async function getLocations() {
+    try {
+      const allLocations = await DataStore.query(Location);
+      //map the name to an array
+      const formatLocations = allLocations.map(({ name: value }) => ({
+        value,
+        label: value,
+      }));
+      setLocations(formatLocations);
+      return true;
+    } catch (error) {
+      setErrorMessage("Error retrieving locations");
+      setShowError(true);
+      //if locations don't load for some reason then we should probably display an error message and route back
+      console.log(error.message);
+      setLoading(false);
+      return false;
+    }
+  }
+
+  async function getPlayers() {
+    let toInvite = [];
+    try {
+      const allPlayers = await DataStore.query(Player, (p) =>
+        p.email.ne(player.email)
+      );
+      allPlayers.map((element) => {
+        toInvite.push({ label: element.name, value: element.id });
+      });
+      setToInvite(toInvite);
+      return true;
+    } catch (error) {
+      setLoading(false);
+      setErrorMessage("Invited invalid player");
+      setShowError(true);
+      console.log("Error occurred in getting all players: " + error.message);
+      return false;
+    }
+  }
+  useEffect(() => {
+    // This needs to be async so we can wait for results before rendering
+    (async () => {
+      setLoading(true);
+      await getPlayers();
+      await getLocations();
+
+      setLoading(false);
+    })();
+  }, []);
 
   //on change function to set user selected date
   const changeSelectedDate = (event, selectedDate) => {
@@ -50,13 +106,14 @@ function UpdateGame({ route, navigation }) {
             (updated.datetime = epochDate);
         })
       );
-      setLoading(false);
-      // navigation.navigate("GameDetails", {game: item , player: player});
+      
+      if (!(await storeGamePlayers(updatedGame.id))) return;
       let item = {
         game: updatedGame,
         player: player,
       };
-
+      
+      setLoading(false);
       navigation.navigate("GameDetails", { item });
     } catch (error) {
       setErrorMessage("Error updating game details");
@@ -64,17 +121,48 @@ function UpdateGame({ route, navigation }) {
       setLoading(false);
       console.log("Error updating game details");
     }
+
+    
+  }
+
+  async function storeGamePlayers(gameId) {
+
+    const filteredToInvite = toInvite.filter((player) => !invitedPlayers.includes(player));
+    
+    for (let i = 0; i < filteredToInvite.length; i++) {
+      try {
+        const gamePlayer = await DataStore.save(
+          new GamePlayer({
+            player_id: filteredToInvite[i],
+            game_id: gameId,
+            rsvp: Rsvp.PENDING,
+            invited: true,
+          })
+        );
+      } catch (error) {
+
+        setShowError(true);
+        console.log(
+          "error: ",
+          error.message,
+          "storing player: ",
+          filteredToInvite[i]
+        );
+        setErrorMessage(`Error storing player: ${filteredToInvite[i]}`);
+        return false;
+      }
+    }
+    return true;
   }
 
   return (
     // This is the create event form
-    <Container
-      loadingState={loading}
-      goBackTo="GameDetails"
-      routeParams={{ item: { game: game, player: player } }}
-    >
+    <Container 
+    loadingState={loading}
+    goBackTo="HomeScreen" 
+    routeParams={{ item: { game: game, player: player } }}>
       <View style={styles.container}>
-        <Text style={styles.text}>Update Your Game</Text>
+        <Text style={styles.text}>Update {gameName}</Text>
         <TextInput
           value={gameName}
           placeholder="Enter a name for the game"
@@ -89,12 +177,7 @@ function UpdateGame({ route, navigation }) {
           value={gameLocation}
           onValueChange={(value) => setLocation(value)}
           placeholder={{ label: "Please select a location", value: null }}
-          items={[
-            { label: "McComas Hall", value: "McComas Hall" },
-            { label: "Lee Courts", value: "Lee Courts" },
-            { label: "Old Blacksburg HS", value: "Old Blacksburg HS" },
-            { label: "Cassell Colisuem", value: "Cassell Coliseum" },
-          ]}
+          items={locations}
           style={customPickerStyles}
         />
         <RNPickerSelect
@@ -109,6 +192,24 @@ function UpdateGame({ route, navigation }) {
           ]}
           style={customPickerStyles}
         />
+        <MultiSelect
+          style={multiSelectStyles.dropdown}
+          placeholderStyle={multiSelectStyles.placeholderStyle}
+          selectedTextStyle={multiSelectStyles.selectedTextStyle}
+          inputSearchStyle={multiSelectStyles.inputSearchStyle}
+          iconStyle={multiSelectStyles.iconStyle}
+          search
+          data={toInvite}
+          labelField="label"
+          valueField="value"
+          placeholder="Invite players"
+          searchPlaceholder="Search..."
+          value={selectedPlayers}
+          onChange={(item) => {
+            setSelected(item);
+          }}
+          selectedStyle={multiSelectStyles.selectedStyle}
+        />
         <Text style={styles.otherText}>Enter date and time for the game</Text>
         <RNDateTimePicker
           mode="datetime"
@@ -116,7 +217,7 @@ function UpdateGame({ route, navigation }) {
           value={chosenDate}
           onChange={changeSelectedDate}
         />
-        <Button title="Update Game" onPress={() => update()} />
+        <Button title="Update Game" onPress={update} />
         {showError && <ErrorPopup errorMessage={errorMessage} />}
       </View>
       <View style={{ flex: 1, backgroundColor: "lightgray" }}></View>
@@ -175,6 +276,43 @@ const customPickerStyles = EStyleSheet.create({
     borderRadius: 8,
     color: "black",
     paddingRight: 30, // to ensure the text is never behind the icon
+  },
+});
+
+const multiSelectStyles = EStyleSheet.create({
+  dropdown: {
+    height: "3rem",
+    width: "80%",
+    borderRadius: "1rem",
+    backgroundColor: "white",
+    margin: "1rem",
+    marginLeft: "1.25 rem",
+    color: "#333",
+    padding: "rem",
+    shadowColor: "#171717",
+    shadowRadius: 3,
+    shadowOpacity: 0.2,
+    shadowOffset: { width: -2, height: 4 },
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 14,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  selectedStyle: {
+    borderRadius: 12,
   },
 });
 
